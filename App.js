70c1f1975ddc0051d788486e821c37c7d3507fb5
Antonia Langer <ai21m032@technikum-wien.at>
2022-09-23 22:12:16 +0200

    Setting up recording logic

--- App.js
+++ App.js
@@ -1,39 +1,108 @@
-import * as React from 'react';
-import { Avatar, Button, Card, Title, Paragraph } from 'react-native-paper';
-import { StyleSheet, Text, View } from 'react-native';
-import AudioRecorderPlayer, { 
-  AVEncoderAudioQualityIOSType,
-  AVEncodingOption, 
-  AudioEncoderAndroidType,
-  AudioSet,
-  AudioSourceAndroidType, 
- } from 'react-native-audio-recorder-player';
+//################### Imports ###########################
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import { Button, StyleSheet, Text, View, Alert } from 'react-native';
+import { Audio } from 'expo-av';
+import * as Sharing from 'expo-sharing';
 
 
-class Application extends React.Component {
-    constructor(props) {
-      super(props);
-      this.state = {
-        isLoggingIn: false,
-        recordSecs: 0,
-        recordTime: '00:00:00',
-        currentPositionSec: 0,
-        currentDurationSec: 0,
-        playTime: '00:00:00',
-        duration: '00:00:00',
-      };
-      this.audioRecorderPlayer = new AudioRecorderPlayer();
-      this.audioRecorderPlayer.setSubscriptionDuration(0.09);
+//################### Implementierung ###################
+export default function App() {
+  const [recording, setRecording] = React.useState();
+  const [recordings, setRecordings] = React.useState([]);
+  const [message, setMessage] = React.useState("");
+
+  async function startRecording() {
+    try {
+      const permission = await Audio.requestPermissionsAsync();
+
+      if (permission.status === "granted") {
+        await Audio.setAudioModeAsync({
+          allowsRecordingIOS: true,
+          playsInSilentModeIOS: true
+        });
+        
+        const { recording } = await Audio.Recording.createAsync(
+          Audio.RECORDING_OPTIONS_PRESET_HIGH_QUALITY
+        );
+
+        setRecording(recording);
+      } else {
+        setMessage("Please grant permission to app to access microphone");
+      }
+    } catch (err) {
+      console.error('Failed to start recording', err);
     }
+  }
 
-    render() {
+  async function stopRecording() {
+    setRecording(undefined);
+    await recording.stopAndUnloadAsync();
+
+    let updatedRecordings = [...recordings];
+    const { sound, status } = await recording.createNewLoadedSoundAsync();
+    updatedRecordings.push({
+      sound: sound,
+      duration: getDurationFormatted(status.durationMillis),
+      file: recording.getURI()
+    });
+
+    console.log({recording})
+
+    setRecordings(updatedRecordings);
+  }
+
+  function getDurationFormatted(millis) {
+    const minutes = millis / 1000 / 60;
+    const minutesDisplay = Math.floor(minutes);
+    const seconds = Math.round((minutes - minutesDisplay) * 60);
+    const secondsDisplay = seconds < 10 ? `0${seconds}` : seconds;
+    return `${minutesDisplay}:${secondsDisplay}`;
+  }
+
+  function getRecordingLines() {
+    return recordings.map((recordingLine, index) => {
       return (
-        <Card>
-        <Card.Title title="Sprachsynthese"/>
-        </Card>
+        <View key={index} style={styles.row}>
+          <Text style={styles.fill}>Recording {index + 1} - {recordingLine.duration}</Text>
+          <Button style={styles.button} onPress={() => recordingLine.sound.replayAsync()} title="Play"></Button>
+          <Button style={styles.button} onPress={() => Sharing.shareAsync(recordingLine.file)} title="Share"></Button>
+        </View>
       );
-    }
+    });
+  }
+
+  return (
+    <View style={styles.container}>
+      <Text>{message}</Text>
+      <Button
+        title={recording ? 'Stop Recording' : 'Start Recording'}
+        onPress={recording ? stopRecording : startRecording} />
+      {getRecordingLines()}
+      <StatusBar style="auto" />
+    </View>
+  );
 }
 
 
-export default Application;
+//################### Styling ###########################
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  row: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  fill: {
+    flex: 1,
+    margin: 16
+  },
+  button: {
+    margin: 16
+  }
+});
