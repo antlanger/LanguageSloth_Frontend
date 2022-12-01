3476308a3c917af82de564b319321cd2a4a1cded
Antonia Langer <ai21m032@technikum-wien.at>
2022-12-01 20:51:12 +0100

    Added Dropdowns

--- TextToSpeech.js
+++ TextToSpeech.js
@@ -4,10 +4,29 @@
 import React, { useState } from 'react';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View, TextInput, Pressable } from 'react-native';
+import {Dropdown} from 'react-native-element-dropdown';
 
 /* -------------------------------------------------------------------------- */
 /*                               Implementation                               */
 /* -------------------------------------------------------------------------- */
+
+/* ------------------------------ Dropdown-Data ----------------------------- */
+const sourceDropdownData = [
+  { label: 'Automatic', value: 'auto'},
+  { label: 'English', value: 'en'},
+  { label: 'German', value: 'de'},
+  { label: 'Spanish', value: 'es'},
+  { label: 'Italian', value: 'it'},
+  { label: 'French', value: 'fr'},
+];
+
+const targetDropdownData = [
+  { label: 'English', value: 'en'},
+  { label: 'German', value: 'de'},
+  { label: 'Spanish', value: 'es'},
+  { label: 'Italian', value: 'it'},
+  { label: 'French', value: 'fr'},
+];
 
 const MultilineTextInput = (props) => {
   return (
@@ -21,34 +40,80 @@ const MultilineTextInput = (props) => {
 
 var translatedText = null;
 
-function handleClick(textValue){
+async function handleClick(textValue, source, target){
 
   if (textValue == null)
   {
     return;
   }
 
-  translatedText = fetch('http://localhost:8080/libretranslate?text=' + textValue, {
+  translatedText = await fetch('http://localhost:8080/libretranslate?text=' + textValue + '&sourceLanguage=' + source + '&targetLanguage=' + target, {
     method: 'GET',
-    mode: 'no-cors',
-  }).then(response => {
-    return response.text;
-  }).then(data => {
-    console.log(data);
-  })
+  }).then(response => response.json())
+  .then(json => {
+    return json;
+  });
 
-  //console.log(translatedText);
+  return translatedText;
+
 }
 
 export default function TextToSpeech() {  
   
 /* -------------------------------- Textinput ------------------------------- */
 const [value, onChangeText] = useState('Enter text here...');
-
-
+const [translation, setTranslation] = useState('');
+const [valueDropSource, setValueSource] = useState(null);
+const [valueDropTarget, setValueTarget] = useState(null);
+const [isFocusSource, setIsFocusSource] = useState(false);
+const [isFocusTarget, setIsFocusTarget] = useState(false);
   
   return (
     <View style={styles.container}>
+        <Dropdown
+          style={[styles.dropdown, isFocusSource && { borderColor: 'blue' }]}
+          placeholderStyle={styles.placeholderStyle}
+          selectedTextStyle={styles.selectedTextStyle}
+          inputSearchStyle={styles.inputSearchStyle}
+          iconStyle={styles.iconStyle}
+          data={sourceDropdownData}
+          search
+          maxHeight={300}
+          labelField="label"
+          valueField="value"
+          placeholder={!isFocusSource ? 'Select source language' : '...'}
+          searchPlaceholder="Search..."
+          value={valueDropSource}
+          onFocus={() => setIsFocusSource(true)}
+          onBlur={() => setIsFocusSource(false)}
+          onChange={item => {
+            setValueSource(item.value);
+            setIsFocusSource(false);
+          }}
+        />
+
+        <Dropdown
+          style={[styles.dropdown, isFocusTarget && { borderColor: 'blue' }]}
+          placeholderStyle={styles.placeholderStyle}
+          selectedTextStyle={styles.selectedTextStyle}
+          inputSearchStyle={styles.inputSearchStyle}
+          iconStyle={styles.iconStyle}
+          data={targetDropdownData}
+          search
+          maxHeight={300}
+          labelField="label"
+          valueField="value"
+          placeholder={!isFocusTarget ? 'Select target language' : '...'}
+          searchPlaceholder="Search..."
+          value={valueDropTarget}
+          onFocus={() => setIsFocusTarget(true)}
+          onBlur={() => setIsFocusTarget(false)}
+          onChange={item => {
+            setValueTarget(item.value);
+            setIsFocusTarget(false);
+          }}
+        />
+
       <MultilineTextInput
         multiline
         numberOfLines={4}
@@ -57,14 +122,14 @@ const [value, onChangeText] = useState('Enter text here...');
         style={styles.input}
       />
 
-      <Pressable style={styles.button} onPress={() => handleClick(value)}>
+      <Pressable style={styles.button} onPress={async () => setTranslation(await handleClick(value, valueDropSource, valueDropTarget))}>
       <Text style={styles.text}>TRANSLATE</Text>
       </Pressable> 
 
       <MultilineTextInput
         multiline
         numberOfLines={4}
-        value={translatedText || ""}
+        value={translation || ""}
         style={styles.input}
       /> 
 
@@ -72,12 +137,6 @@ const [value, onChangeText] = useState('Enter text here...');
   );
 }
 
-
-/*
-<Pressable style={styles.button}>
-          <Text style={styles.text}>This is a button!</Text>
-        </Pressable>
-*/
 
 
 /* -------------------------------------------------------------------------- */
@@ -113,5 +172,32 @@ const styles = StyleSheet.create({
     fontWeight: 'bold',
     letterSpacing: 0.25,
     color: 'white',
-  }
+  },
+  dropdown: {
+    height: 50,
+    borderColor: 'gray',
+    borderWidth: 0.5,
+    borderRadius: 8,
+    paddingHorizontal: 8,
+    margin: 10
+  },
+  placeholderStyle: {
+    fontSize: 16,
+    alignItems: 'center',
+    justifyContent: 'center',
+    paddingVertical: 20,
+    paddingHorizontal: 35
+  },
+  selectedTextStyle: {
+    fontSize: 16,
+    alignItems: 'center',
+    justifyContent: 'center',
+    paddingVertical: 20,
+    width: 185,
+    textAlign: 'center',
+  },
+  inputSearchStyle: {
+    height: 40,
+    fontSize: 16,
+  },
 });
