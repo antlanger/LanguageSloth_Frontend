3fc3e77b1e41414da4a6a095e5f63fb9c0c5d040
Antonia Langer <ai21m032@technikum-wien.at>
2023-01-14 19:56:54 +0100

    Formatted file and added new dropdown

--- SpeechToSpeech.js
+++ SpeechToSpeech.js
@@ -1,87 +1,88 @@
 /* -------------------------------------------------------------------------- */
 /*                                   Imports                                  */
 /* -------------------------------------------------------------------------- */
-import React, { useState } from 'react';
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, Pressable, Image, ImageBackground} from 'react-native';
-import {Dropdown} from 'react-native-element-dropdown';
+import React, { useState } from "react";
+import { StatusBar } from "expo-status-bar";
+import {
+  StyleSheet,
+  Text,
+  View,
+  Pressable,
+  Image,
+  ImageBackground,
+} from "react-native";
+import { Dropdown } from "react-native-element-dropdown";
 
 let mediaRecorder;
 let chunks = [];
 
-
 /* -------------------------------------------------------------------------- */
 /*                               Implementation                               */
 /* -------------------------------------------------------------------------- */
-export default function SpeechToSpeech() { 
+export default function SpeechToSpeech() {
   const [recording, setRecording] = React.useState();
   const [play, setPlayButton] = React.useState();
   //const [recordings, setRecordings] = React.useState([]);
   const [message, setMessage] = React.useState("");
   const [audioUrl, setAudioUrl] = React.useState("");
-  
 
   /* -------------------------------- Recording ------------------------------- */
   async function startRecording() {
-    if (navigator.mediaDevices.getUserMedia)
-    {
-      console.log('SpeechToSpeech: getUserMedia supported.');
-      const constraints = {audio: true};
+    if (navigator.mediaDevices.getUserMedia) {
+      console.log("SpeechToSpeech: getUserMedia supported.");
+      const constraints = { audio: true };
 
-      navigator.mediaDevices
-        .getUserMedia(constraints)
-        .then((stream) => {
-          mediaRecorder = new MediaRecorder(stream);
+      navigator.mediaDevices.getUserMedia(constraints).then((stream) => {
+        mediaRecorder = new MediaRecorder(stream);
 
-          mediaRecorder.ondataavailable = (e) => {
-            console.log(e.data)
-            chunks.push(e.data);
+        mediaRecorder.ondataavailable = (e) => {
+          console.log(e.data);
+          chunks.push(e.data);
+        };
+
+        mediaRecorder.start();
+
+        mediaRecorder.onstop = (e) => {
+          console.log("Creating BLOB...");
+          const audioBlob = new Blob(chunks, { type: "audio/webm" });
+          setAudioUrl(URL.createObjectURL(audioBlob));
+          //const audio = new Audio(audioUrl);
+          //audio.play();
+
+          console.log("Clearing chunks...");
+          chunks = [];
+
+          console.log("Attaching BLOB to FormData...");
+          const formData = new FormData();
+          formData.append("file", audioBlob);
+
+          for (var key of formData.entries()) {
+            console.log(key[0] + "," + key[1]);
+          }
+
+          console.log("Sending BLOB to server for further processing...");
+          const options = {
+            method: "POST",
+            body: formData,
           };
-          
-          mediaRecorder.start();
 
-          mediaRecorder.onstop = (e) => {
-            console.log('Creating BLOB...');
-            const audioBlob = new Blob(chunks, { type: "audio/webm"});
-            setAudioUrl(URL.createObjectURL(audioBlob));
-            //const audio = new Audio(audioUrl);
-            //audio.play();
-            
-            console.log('Clearing chunks...');
-            chunks = [];
-
-            console.log('Attaching BLOB to FormData...');
-            const formData = new FormData();
-            formData.append('file', audioBlob);
-
-            for (var key of formData.entries()){
-              console.log(key[0] + ',' + key[1]);
-            }
-
-            console.log('Sending BLOB to server for further processing...');
-            const options = {
-              method: 'POST',
-              body: formData
-            }
-
-            fetch('http://localhost:8080/convertInputToWav', options)
-            .then(resp => resp.blob())
-            .then(data => {
-              console.log('Return data ' + data);
+          fetch("http://localhost:8080/convertInputToWav", options)
+            .then((resp) => resp.blob())
+            .then((data) => {
+              console.log("Return data " + data);
               //var binaryData = [];
               //binaryData.push(data.body);
               //var data_blob = new Blob(data, {type: "audio/wav"})
               setAudioUrl(URL.createObjectURL(data));
             })
-            .catch(error => {
+            .catch((error) => {
               console.error(error);
             });
-          }
+        };
 
-          console.log('MediaRecorder state is ' + mediaRecorder.state);
-          console.log('Recording started...');
-        })
-
+        console.log("MediaRecorder state is " + mediaRecorder.state);
+        console.log("Recording started...");
+      });
     }
     setRecording(true);
     setPlayButton(false);
@@ -89,45 +90,40 @@ export default function SpeechToSpeech() {
 
   async function stopRecording() {
     mediaRecorder.stop();
-    console.log('Recording stopped...');    
+    console.log("Recording stopped...");
     setRecording(false);
     setPlayButton(true);
   }
 
   function setButtonText() {
-    console.log('Variable - recording - is ' + recording);
-    console.log('Variable - play - is ' + play);
+    console.log("Variable - recording - is " + recording);
+    console.log("Variable - play - is " + play);
 
     if (!recording && !play) {
-      console.log('START');
-      return 'START RECORDING';
-    }else if (!recording && play)
-    {
-      console.log('PLAY');
-      return 'PLAY AUDIO';
-    }else if (recording)
-    {
-      console.log('STOP');
-      return 'STOP RECORDING';
+      console.log("START");
+      return "START RECORDING";
+    } else if (!recording && play) {
+      console.log("PLAY");
+      return "PLAY AUDIO";
+    } else if (recording) {
+      console.log("STOP");
+      return "STOP RECORDING";
     }
   }
 
   function workOnAction() {
-
     if (recording == undefined || play == undefined) {
       setRecording(false);
       setPlayButton(false);
     }
-    
+
     if (!recording && !play) {
       startRecording();
-    }else if (!recording && play)
-    {
+    } else if (!recording && play) {
       // Play received audio
       let audio = new Audio(audioUrl);
       audio.play();
-    }else if (recording)
-    {
+    } else if (recording) {
       stopRecording();
     }
   }
@@ -140,37 +136,47 @@ export default function SpeechToSpeech() {
   function showDeleteButton() {
     if (play) {
       return (
-          <Pressable onPress={redoRecording}>
-            <Image style={styles.deleteButton} source={require('./images/LanguageSloth_Redo.svg')}/>
-          </Pressable>
+        <Pressable onPress={redoRecording}>
+          <Image
+            style={styles.deleteButton}
+            source={require("./images/LanguageSloth_Redo.svg")}
+          />
+        </Pressable>
       );
     } else {
-        return null;
+      return null;
     }
   }
 
   /* -------------------------------- Dropdown -------------------------------- */
   const dropdownData = [
-    { label: 'English', value: '1'},
-    { label: 'German', value: '2'},
-    { label: 'Spanish', value: '3'},
-    { label: 'Italian', value: '4'},
-    { label: 'French', value: '5'}
+    { label: "English", value: "1" },
+    { label: "German", value: "2" },
+    { label: "Spanish", value: "3" },
+    { label: "Italian", value: "4" },
+    { label: "French", value: "5" },
   ];
 
-  const [value, setValue] = useState(null);
-  const [isFocus, setIsFocus] = useState(false);
+  const [inputValue, setInputValue] = useState(null);
+  const [isInputFocus, setIsInputFocus] = useState(false);
 
-  //<Button style={styles.button} onPress={() => Sharing.shareAsync(recordingLine.file)} title="Share"></Button>
-    
-  
+  const [outputValue, setOutputValue] = useState(null);
+  const [isOutputFocus, setIsOutputFocus] = useState(false);
+
+
+  /* -------------------------------------------------------------------------- */
+  /*                                  Rendering                                 */
+  /* -------------------------------------------------------------------------- */
 
   return (
     <View style={styles.background}>
-        <Image style={styles.backgroundImage} source={require('./images/LanguageSloth_Background.png')}/>
-        <View style={styles.container}>
-      <Dropdown
-          style={[styles.dropdown, isFocus && { borderColor: 'blue' }]}
+      <Image
+        style={styles.backgroundImage}
+        source={require("./images/LanguageSloth_Background.png")}
+      />
+      <View style={styles.container}>
+        <Dropdown
+          style={[styles.dropdown, isInputFocus && { borderColor: "blue" }]}
           placeholderStyle={styles.placeholderStyle}
           selectedTextStyle={styles.selectedTextStyle}
           inputSearchStyle={styles.inputSearchStyle}
@@ -180,41 +186,51 @@ export default function SpeechToSpeech() {
           maxHeight={300}
           labelField="label"
           valueField="value"
-          placeholder={!isFocus ? 'Select language' : '...'}
+          placeholder={!isInputFocus ? "Select input language" : "..."}
           searchPlaceholder="Search..."
-          value={value}
-          onFocus={() => setIsFocus(true)}
-          onBlur={() => setIsFocus(false)}
-          onChange={item => {
-            setValue(item.value);
-            setIsFocus(false);
+          value={inputValue}
+          onFocus={() => setIsInputFocus(true)}
+          onBlur={() => setIsInputFocus(false)}
+          onChange={(item) => {
+            setInputValue(item.value);
+            setIsInputFocus(false);
           }}
         />
 
+        <Dropdown
+          style={[styles.dropdown, isOutputFocus && { borderColor: "blue" }]}
+          placeholderStyle={styles.placeholderStyle}
+          selectedTextStyle={styles.selectedTextStyle}
+          inputSearchStyle={styles.inputSearchStyle}
+          iconStyle={styles.iconStyle}
+          data={dropdownData}
+          search
+          maxHeight={300}
+          labelField="label"
+          valueField="value"
+          placeholder={!isOutputFocus ? "Select output language" : "..."}
+          searchPlaceholder="Search..."
+          value={outputValue}
+          onFocus={() => setIsOutputFocus(true)}
+          onBlur={() => setIsOutputFocus(false)}
+          onChange={(item) => {
+            setOutputValue(item.value);
+            setIsOutputFocus(false);
+          }}
+        />
 
-      <Text>{message}</Text>
+        <Text>{message}</Text>
         <View style={styles.buttonView}>
-          <Pressable
-          onPress=
-          {
-            workOnAction
-          }
-          style={styles.button}> 
-              <Text style={styles.text}>
-                {
-                  setButtonText()
-                }
-              </Text>
+          <Pressable onPress={workOnAction} style={styles.button}>
+            <Text style={styles.text}>{setButtonText()}</Text>
           </Pressable>
           {showDeleteButton()}
         </View>
-      <StatusBar style="auto" />
+        <StatusBar style="auto" />
+      </View>
     </View>
-    </View>
-    
   );
 }
-
 
 /* -------------------------------------------------------------------------- */
 /*                                   Styling                                  */
@@ -222,58 +238,58 @@ export default function SpeechToSpeech() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
+    alignItems: "center",
+    justifyContent: "center",
     zIndex: 2,
   },
   row: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    justifyContent: 'center',
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "center",
   },
   fill: {
     flex: 1,
-    margin: 16
+    margin: 16,
   },
   button: {
-    alignItems: 'center',
-    justifyContent: 'center',
+    alignItems: "center",
+    justifyContent: "center",
     paddingVertical: 12,
     paddingHorizontal: 32,
     borderRadius: 4,
     elevation: 3,
-    backgroundColor: 'tomato',
+    backgroundColor: "tomato",
   },
   text: {
     fontSize: 16,
     lineHeight: 21,
-    fontWeight: 'bold',
+    fontWeight: "bold",
     letterSpacing: 0.25,
-    color: 'white',
+    color: "white",
   },
   dropdown: {
     height: 50,
-    borderColor: 'gray',
+    borderColor: "gray",
     borderWidth: 0.5,
     borderRadius: 8,
     paddingHorizontal: 8,
     margin: 10,
-    backgroundColor: '#fff'
+    backgroundColor: "#fff",
   },
   placeholderStyle: {
     fontSize: 16,
-    alignItems: 'center',
-    justifyContent: 'center',
+    alignItems: "center",
+    justifyContent: "center",
     paddingVertical: 20,
-    paddingHorizontal: 35
+    paddingHorizontal: 35,
   },
   selectedTextStyle: {
     fontSize: 16,
-    alignItems: 'center',
-    justifyContent: 'center',
+    alignItems: "center",
+    justifyContent: "center",
     paddingVertical: 20,
     width: 185,
-    textAlign: 'center',
+    textAlign: "center",
   },
   inputSearchStyle: {
     height: 40,
@@ -282,24 +298,24 @@ const styles = StyleSheet.create({
   deleteButton: {
     width: 30,
     height: 30,
-    marginLeft: 10
+    marginLeft: 10,
   },
   buttonView: {
-    alignItems: 'center',
-    justifyContent: 'center',
-    flexDirection: 'row',
+    alignItems: "center",
+    justifyContent: "center",
+    flexDirection: "row",
   },
   backgroundImage: {
     width: 400,
     height: 400,
-    position: 'absolute',
+    position: "absolute",
     right: -25,
     top: -25,
-    zIndex: 1
+    zIndex: 1,
   },
   background: {
-    width: '100%',
-    height: '100%',
-    backgroundColor: '#fff',
-  }
+    width: "100%",
+    height: "100%",
+    backgroundColor: "#fff",
+  },
 });
